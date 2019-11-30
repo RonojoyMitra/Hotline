558b8c5ba0f8429055d2cda8535a70fd47a8cd78
RePleX49 <lukelin2009@hotmail.com>
2019-11-30 14:23:16 -0500

    Rough working PointSystem

--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -1,35 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PointManager : MonoBehaviour
 {
     int ComboCount;
     public int totalScore;
+
+    [SerializeField] GameObject playerChar;
+
     [SerializeField] float ComboTimeFrame = 2.0f;
-    [SerializeField] GameObject pointText;
+    [SerializeField] GameObject pointTextPrefab;
+    [SerializeField] GameObject comboTextPrefab;
+    [SerializeField] TextMeshProUGUI totalScoreText;
     [SerializeField] GameObject textCanvas;
+    [SerializeField] GameObject floatingCanvasPrefab;
+
+    [SerializeField] int EnemyPointsValue = 300;
+    [SerializeField] float comboMultiplier = 1.25f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if(!pointTextPrefab)
+        {
+            Debug.Log("Pop up text prefab was not assigned in PointManager object");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(!totalScoreText)
+        {
+            Debug.Log("Total Score text was not assigned in PointManager object");
+        }
     }
 
     public void AddPoints(Vector3 pointLocation)
     {
-        Vector3 ScreenLocation = Camera.main.WorldToScreenPoint(pointLocation);
+        // Create temporary canvas for floating point text, and create point text object       
+        GameObject floatingCanvas = Instantiate(floatingCanvasPrefab);
+        floatingCanvas.transform.position = Camera.main.transform.position - (Vector3.down);
+        GameObject textObject = Instantiate(pointTextPrefab);
 
-        GameObject textObject = Instantiate(pointText);
-        textObject.transform.SetParent(textCanvas.transform);
+        // Set a bunch of transform parameters so that point text will appear on screen properly
+        textObject.transform.SetParent(floatingCanvas.transform);
+        textObject.transform.rotation = textObject.transform.parent.rotation;
+        textObject.transform.localScale = textObject.transform.parent.localScale * 5;
 
-        textObject.transform.position = ScreenLocation;
+        // TODO fix position y to prevent clipping
+        textObject.transform.position = pointLocation;
+
+        Destroy(floatingCanvas, 1.25f);
+
+        totalScore += EnemyPointsValue;
+        totalScoreText.text = totalScore.ToString() + "PTS";
     }
 
     public void AddComboCount()
@@ -49,11 +73,33 @@ public class PointManager : MonoBehaviour
 
     void CountCombo()
     {
-        Debug.Log(ComboCount);
-        //Show combo text fx
+        // Debug.Log(ComboCount);
+
+        GameObject floatingCanvas = Instantiate(floatingCanvasPrefab);
+        floatingCanvas.transform.position = Camera.main.transform.position - (Vector3.down);
+
+        GameObject textObject = Instantiate(comboTextPrefab);
+        textObject.transform.SetParent(floatingCanvas.transform);
+
+        // Set a bunch of transform parameters so that text will appear on screen properly
+        textObject.transform.SetParent(floatingCanvas.transform);
+        textObject.transform.rotation = textObject.transform.parent.rotation;
+        textObject.transform.localScale = textObject.transform.parent.localScale * 5;
+        textObject.transform.position = playerChar.transform.position;
+
+        Destroy(textObject, 1.25f);
+
+        // calculate combo total
+        int totalComboPoints = (int)(EnemyPointsValue * ComboCount * comboMultiplier);
+
+        // assign combo count to text
+        // TODO assign combo value to text as well
+        textObject.GetComponent<TextMeshProUGUI>().text = ComboCount + "X COMBO";
 
         //Add to overall score
+        totalScore += totalComboPoints;
 
+        // Reset Combo Count
         ComboCount = 0;
     }
 }
