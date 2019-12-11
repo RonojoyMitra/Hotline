a3eaabbfe1308de5459e6c4b1301645236cfafc2
Ronojoy Mitra <ronojoymitra@gmail.com>
2019-12-11 00:33:32 -0500

    Added Scripts and enemies to cutscene

---
+++ Assets/DirectorSceneSwitchScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class DirectorSceneSwitchScript : MonoBehaviour
+{
+    public PlayableDirector director;
+
+    void OnEnable()
+    {
+        director.stopped += OnPlayableDirectorStopped;
+    }
+
+    void OnPlayableDirectorStopped(PlayableDirector aDirector)
+    {
+        if (director == aDirector)
+            Debug.Log("PlayableDirector named " + aDirector.name + " is now stopped.");
+    }
+
+    void OnDisable()
+    {
+        director.stopped -= OnPlayableDirectorStopped;
+    }
+}
